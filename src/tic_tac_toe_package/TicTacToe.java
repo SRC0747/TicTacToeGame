83921690d7799ec1b49fa71dc223a4447ec80d46
SRC0747 <srcsrk1998@gmail.com>
2021-08-10 09:01:30 +0530

    added: create empty board for tic-tac-toe game.

--- src/tic_tac_toe_package/TicTacToe.java
+++ src/tic_tac_toe_package/TicTacToe.java
@@ -1,8 +1,15 @@
 package tic_tac_toe_package;
 
 public class TicTacToe {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to Tic-tac-Toe Game.");
+        createBoard();
+    }
+
+    private static void createBoard() {
+        char[] board = new char [10];
+        for (int index= 1; index < board.length; index++) {
+            System.out.println(board[index]);
+        }
     }
 }
